20bf6a96be024db522dee226274f9fa1c07a1d15
Vladimir <turkonjavlada@gmail.com>
2021-04-11 15:58:56 +0200

    Add startup files

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+import { app } from './app';
+
+const start = async () => {
+  const { TICKETS_MONGO_URI, JWT_KEY } = process.env;
+
+  if (!JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
+
+  if (!TICKETS_MONGO_URI) {
+    throw new Error('TICKETS_MONGO_URI must be defined');
+  }
+
+  try {
+    await mongoose.connect(TICKETS_MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log('Tickets service connected to mongodb');
+  } catch (error) {
+    console.error('Error connecting to mongodb: ', error.message);
+  }
+  app.listen(3000, () => console.log(`Ready at http://locahost:3000`));
+};
+
+start();
