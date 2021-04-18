dcaaf70cfea504df23de1c586c8cd9567f31db97
Vladimir <turkonjavlada@gmail.com>
2021-04-18 11:34:23 +0200

    Add first test and route

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import { json } from 'body-parser';
 
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError } from '@vttickets/common';
+import { createTicketRouter } from './routes/new';
 
 const app = express();
 app.set('trust proxy', true);
@@ -14,6 +15,8 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
+
+app.use(createTicketRouter);
 
 app.all('*', async (req, res, next) => {
   throw new NotFoundError();
