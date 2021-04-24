a2113e71a620ceb27b559e26877d55d661b05db0
Vladimir <turkonjavlada@gmail.com>
2021-04-24 18:24:54 +0200

    Connect show route in app.ts

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError, currentUser } from '@vttickets/common';
 import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
 
 const app = express();
 app.set('trust proxy', true);
@@ -18,6 +19,7 @@ app.use(
 app.use(currentUser);
 
 app.use(createTicketRouter);
+app.use(showTicketRouter);
 
 app.all('*', async (req, res, next) => {
   throw new NotFoundError();
