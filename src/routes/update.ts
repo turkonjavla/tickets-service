f53f52801368292ab9b2a6f688efd8689457da72
Vladimir Turkonja <turkonjavlada@gmail.com>
2021-05-09 16:10:16 +0200

    Add validation and update logic when updating a ticket

--- src/routes/update.ts
+++ src/routes/update.ts
@@ -2,6 +2,7 @@ import {
   NotAuthorizedError,
   NotFoundError,
   requireAuth,
+  validateRequest,
 } from '@vttickets/common';
 import { Request, Response, Router } from 'express';
 import { body } from 'express-validator';
@@ -12,7 +13,13 @@ const router = Router();
 router.put(
   '/api/tickets/:id',
   requireAuth,
-  [],
+  [
+    body('title').not().isEmpty().withMessage('Title is required'),
+    body('price')
+      .isFloat({ gt: 0 })
+      .withMessage('Price must be provided and greater than  0'),
+  ],
+  validateRequest,
   async (req: Request, res: Response) => {
     const id = req.params.id;
     const ticket = await Ticket.findById(id);
@@ -24,6 +31,12 @@ router.put(
     if (ticket.userId !== req.currentUser!.id) {
       throw new NotAuthorizedError();
     }
+
+    ticket.set({
+      title: req.body.title,
+      price: req.body.price,
+    });
+    await ticket.save();
 
     res.send(ticket);
   }
