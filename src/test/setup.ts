272844be24264e3ca474ca74f9ede50ec0a2ab7e
Vladimir Turkonja <turkonjavlada@gmail.com>
2021-05-09 15:30:42 +0200

    Change authenticated user Id to test update ticket route

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -39,7 +39,7 @@ afterAll(async () => {
 global.getAuthCookie = () => {
   // build a JWT payload { id, email }
   const payload = {
-    id: '876saga5',
+    id: new mongoose.Types.ObjectId().toHexString(),
     email: 'test@test.com',
   };
 
