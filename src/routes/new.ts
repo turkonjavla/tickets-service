174b8f2ed98d7eb3564002b01ad4f976f2076be0
Vladimir Turkonja <turkonjavlada@gmail.com>
2021-05-22 11:58:30 +0200

    Use TicketCreatedPublisher when creating a new ticket

--- src/routes/new.ts
+++ src/routes/new.ts
@@ -1,7 +1,9 @@
 import { requireAuth, validateRequest } from '@vttickets/common';
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
 import { Ticket } from '../models/ticket';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -20,6 +22,13 @@ router.post(
     const ticket = Ticket.build({ title, price, userId: req.currentUser!.id });
 
     await ticket.save();
+
+    await new TicketCreatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
     res.status(201).send(ticket);
   }
 );
