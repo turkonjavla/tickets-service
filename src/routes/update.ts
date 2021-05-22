8030c0a338a0d45966bc8a546cc07275b89f3e7a
Vladimir Turkonja <turkonjavlada@gmail.com>
2021-05-22 14:16:46 +0200

    Modify update.ts to support event publishing on ticket update

--- src/routes/update.ts
+++ src/routes/update.ts
@@ -6,7 +6,9 @@ import {
 } from '@vttickets/common';
 import { Request, Response, Router } from 'express';
 import { body } from 'express-validator';
+import { TicketUpdatedPublisher } from '../events/publishers/ticket-updated-publisher';
 import { Ticket } from '../models/ticket';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = Router();
 
@@ -37,6 +39,13 @@ router.put(
       price: req.body.price,
     });
     await ticket.save();
+
+    new TicketUpdatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
 
     res.send(ticket);
   }
