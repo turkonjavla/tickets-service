ec47336b393d9f240f714e05e1e05bb63bb2ba79
Vladimir Turkonja <turkonjavlada@gmail.com>
2021-05-23 14:52:32 +0200

    Uppercase nats string

--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,7 @@ const start = async () => {
     await natsWrapper.connect(NATS_CLUSTER_ID, NATS_CLIENT_ID, NATS_URL);
 
     natsWrapper.client.on('close', () => {
-      console.log('Nats connection closed');
+      console.log('NATS connection closed');
       process.exit();
     });
     process.on('SIGINT', () => natsWrapper.client.close());
